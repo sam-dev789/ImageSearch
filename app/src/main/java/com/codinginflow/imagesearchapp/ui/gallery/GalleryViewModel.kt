23ddc07dd2764876a82b5094e89ae97059e8642e
sam-dev789 <slsedivy@gmail.com>
2020-12-04 16:07:01 -0500

    Handling process death

--- app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/GalleryViewModel.kt
@@ -1,26 +1,29 @@
 package com.codinginflow.imagesearchapp.ui.gallery
 
+import androidx.hilt.Assisted
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.switchMap
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import androidx.paging.cachedIn
 import com.codinginflow.imagesearchapp.data.UnsplashRepository
 
-class GalleryViewModel @ViewModelInject constructor(private val repository: UnsplashRepository): ViewModel() {
+class GalleryViewModel @ViewModelInject constructor(
+    private val repository: UnsplashRepository,
+    @Assisted state: SavedStateHandle
+) :
+    ViewModel() {
 
-    private val currentQuery = MutableLiveData(DEFAULT_QUERY)
+    private val currentQuery = state.getLiveData(CURRENT_QUERY, DEFAULT_QUERY)
 
     val photos = currentQuery.switchMap { queryString ->
         repository.getSearchResults(queryString).cachedIn(viewModelScope)
     }
 
-    fun searchPhotos(query: String){
+    fun searchPhotos(query: String) {
         currentQuery.value = query
     }
 
     companion object {
         private const val DEFAULT_QUERY = "dogs"
+        private const val CURRENT_QUERY = "current_query"
     }
 }
